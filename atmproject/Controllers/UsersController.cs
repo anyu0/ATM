96a42ecc7d0e037bde0232ba43c949c26a0bcdfe
Junghoo Kim (Andy) <kimj0@sewanee.edu>
2019-02-23 21:31:31 -0500

    Finished the project

---
+++ atmproject/Controllers/UsersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using atmproject.Models;
+
+namespace atmproject.Controllers
+{
+    public class UsersController : Controller
+    {
+        private AccountDBEntities db = new AccountDBEntities();
+        
+
+        // GET: Users/Deposit/5
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "AccountNumber,Balance")] Users users, string account)
+        {
+            return RedirectToAction("Balance", new { id = account });
+        }
+
+
+        // GET: Users/Balance/5
+        public ActionResult Balance(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // GET: Users/Deposit/5
+        public ActionResult Deposit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+        // POST: Users/Deposit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Deposit([Bind(Include = "AccountNumber,Balance")] Users users, int moneyinput)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Balance += moneyinput;
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Balance", new { id = users.AccountNumber } );
+            }
+            return View(users);
+        }
+        // GET: Users/Withdrawal/5
+        public ActionResult Withdrawal(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+        // POST: Users/Withdrawal/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Withdrawal([Bind(Include = "AccountNumber,Balance")] Users users, int moneyinput)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Balance -= moneyinput;
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Balance", new { id = users.AccountNumber });
+            }
+            return View(users);
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
