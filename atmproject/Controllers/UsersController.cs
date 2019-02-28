22cf3fb780561c6bff0f125bb05cf8fda11878b4
Junghoo Kim (Andy) <kimj0@sewanee.edu>
2019-02-27 20:13:02 -0500

    Error checking

--- atmproject/Controllers/UsersController.cs
+++ atmproject/Controllers/UsersController.cs
@@ -26,6 +26,16 @@ namespace atmproject.Controllers
         {
             return RedirectToAction("Balance", new { id = account });
         }
+        public ActionResult IndexError()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult IndexError([Bind(Include = "AccountNumber,Balance")] Users users, string account)
+        {
+            return RedirectToAction("Balance", new { id = account });
+        }
 
 
         // GET: Users/Balance/5
@@ -33,12 +43,12 @@ namespace atmproject.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("IndexError");
             }
             Users users = db.Users.Find(id);
             if (users == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("IndexError");
             }
             return View(users);
         }
@@ -48,12 +58,12 @@ namespace atmproject.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("IndexError");
             }
             Users users = db.Users.Find(id);
             if (users == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("IndexError");
             }
             return View(users);
         }
@@ -78,12 +88,12 @@ namespace atmproject.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("IndexError");
             }
             Users users = db.Users.Find(id);
             if (users == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("IndexError");
             }
             return View(users);
         }
@@ -96,6 +106,10 @@ namespace atmproject.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (moneyinput % 20 != 0 || users.Balance < moneyinput)
+                {
+                    return RedirectToAction("WithdrawalError", new { id = users.AccountNumber });
+                }
                 users.Balance -= moneyinput;
                 db.Entry(users).State = EntityState.Modified;
                 db.SaveChanges();
@@ -104,7 +118,42 @@ namespace atmproject.Controllers
             return View(users);
         }
 
-        
+        // GET: Users/Withdrawal/5
+        public ActionResult WithdrawalError(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("IndexError");
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return RedirectToAction("IndexError");
+            }
+            return View(users);
+        }
+        // POST: Users/Withdrawal/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult WithdrawalError([Bind(Include = "AccountNumber,Balance")] Users users, int moneyinput)
+        {
+            if (ModelState.IsValid)
+            {
+                if (moneyinput % 20 != 0 || users.Balance < moneyinput)
+                {
+                    return RedirectToAction("WithdrawalError", new { id = users.AccountNumber });
+                }
+                users.Balance -= moneyinput;
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Balance", new { id = users.AccountNumber });
+            }
+            return View(users);
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
